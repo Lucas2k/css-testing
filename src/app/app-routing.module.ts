5a7ed3fb7de784ddebed01113fd7a2c778138f1a
Lucas Granda <hlucas@migraciones.gob.pe>
2019-08-06 17:04:23 -0500

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Testing1Component } from './css-grid-layout/testing1/testing1.component';
 
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  { path: 'testing1', component: Testing1Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
